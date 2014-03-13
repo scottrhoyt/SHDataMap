863b3153fe18f1d9e46c77ef35efa3470e4b3763
Scott Hoyt <scottrhoyt@gmail.com>
2014-03-13 18:58:18 -0500

    Initial commit.

---
+++ SHDataMap/SHDataMapContoller.h
@@ -0,0 +1,16 @@
+//
+//  SHDataMapContoller.h
+//  SHDataMap
+//
+//  Created by Scott Hoyt on 3/13/14.
+//  Copyright (c) 2014 Wild Onion Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface SHDataMapContoller : UIViewController
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
---
+++ SHDataMap/SHDataMapContoller.m
@@ -0,0 +1,17 @@
+//
+//  SHDataMapContoller.m
+//  SHDataMap
+//
+//  Created by Scott Hoyt on 3/13/14.
+//  Copyright (c) 2014 Wild Onion Labs. All rights reserved.
+//
+
+#import "SHDataMapContoller.h"
+
+@interface SHDataMapContoller ()
+
+@end
+
+@implementation SHDataMapContoller
+
+@end
