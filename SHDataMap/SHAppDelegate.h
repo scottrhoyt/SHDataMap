c29494723e602478fdefd3bf28f693deb4f43784
Scott Hoyt <scottrhoyt@gmail.com>
2014-03-13 18:54:23 -0500

    Initial Commit

---
+++ SHDataMap/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  SHDataMap
+//
+//  Created by Scott Hoyt on 3/13/14.
+//  Copyright (c) 2014 Wild Onion Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SHDataMap/SHViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHViewController.h
+//  SHDataMap
+//
+//  Created by Scott Hoyt on 3/13/14.
+//  Copyright (c) 2014 Wild Onion Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHViewController : UIViewController
+
+@end
